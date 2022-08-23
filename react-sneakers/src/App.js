db556ad96c81fb665c992a69668eb4979a97402f
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2022-08-23 23:26:24 +0300

    day

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -3,6 +3,22 @@
 function App() {
   return (
       <div className="wrapper">
+          <header>
+              <div>
+                  <svg />
+                  <h3>REACT SNEAKERS</h3>
+                  <p>Магазин лучших кросовок</p>
+              </div>
+              <ul>
+                  <li>
+                      <svg/>
+                      <span>1205 руб.</span>
+                  </li>
+                  <li>
+                      <svg  />
+                  </li>
+              </ul>
+          </header>
       </div>
   );
 }
