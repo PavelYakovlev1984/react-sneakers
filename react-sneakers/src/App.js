9d1b1be426a2b7af763176579f10645a8bfb9946
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2022-08-24 19:05:54 +0300

    day 3

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -3,28 +3,38 @@ import React from "react";
 
 function App() {
   return (
-      <div className="wrapper">
-          <header>
-              <div className="headerLeft">
+      <div className="wrapper clear">
+          <header className="d-flex justify-between align-center p-40">
+              <div className="d-flex align-center">
                   <img width={40} height={40} src="/img/logo.svg"/>
-                  <div className="headerInfo">
-                      <h3>REACT SNEAKERS</h3>
+                  <div>
+                      <h3 className="text-uppercase">Reart Sneakers</h3>
                       <p>Магазин лучших кросовок</p>
                   </div>
               </div>
-              <ul className="headerRight">
-                  <li>
-                      <img width={40} height={40} src="/img/cart.svg"/>
+              <ul className="d-flex">
+                  <li className="mr-30">
+                      <img width={18} height={18} src="/img/cart.svg"/>
                       <span>1205 руб.</span>
                   </li>
                   <li>
-                      <img width={40} height={40} src="/img/user.svg"/>
+                      <img width={18} height={18} src="/img/user.svg"/>
                   </li>
               </ul>
           </header>
-          <div className="content">
-              <h1>Все крсовки</h1>
-              ......
+          <div className="content p-40">
+              <h1>Все кроссовки</h1>
+              <div className="card">
+                  <img src="" alt=""/>
+                  <p>   </p>
+                  <div className="">
+                      <span>Цена:</span>
+                      <b>12 999 руб.</b>
+                  </div>
+                  <button>
+                      <img width={11} height={11} src="/public/img" alt="Plus"/>
+                  </button>
+              </div>
           </div>
       </div>
   );
