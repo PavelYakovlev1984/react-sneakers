6cb47751b86cd9aabc02a546933beed502b7ecb5
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2022-08-28 14:26:05 +0300

    button

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -7,15 +7,36 @@ function App() {
           <div className="overlay">
               <div className="drawer">
                   <h2 className="mb-30">Корзина</h2>
-                  <div className="cartItem d-flex align-center">
-                      <div className="cartItemImg" style={{
-                          backgroundImage: 'url(img/sneakers/1.jpg)'
-                      }}></div>
-                      <div>
-                          <p className="mb-15">Мужские Кроссовки Nike Air Max 270</p>
-                          <b>12 999 руб.</b>
-                      </div>
-                      <img className="removeBtn" src="img/sneakers/close.svg" alt="Close"/></div>
+                  <div className="items">
+                      <div className="cartItem d-flex align-center mb-20">
+                          <div className="cartItemImg" style={{
+                              backgroundImage: 'url(img/sneakers/1.jpg)'
+                          }}></div>
+                          <div>
+                              <p className="mb-15 mr-15">Мужские Кроссовки <br/>Nike Air Max 270</p>
+                              <b>12 999 руб.</b>
+                          </div>
+                          <img className="removeBtn" src="img/sneakers/close.svg" alt="Close"/></div>
+                      <div className="cartItem d-flex align-center mb-20">
+                          <div className="cartItemImg" style={{
+                              backgroundImage: 'url(img/sneakers/1.jpg)'
+                          }}></div>
+                          <div>
+                              <p className="mb-15 mr-15">Мужские Кроссовки <br/>Nike Air Max 270</p>
+                              <b>12 999 руб.</b>
+                          </div>
+                          <img className="removeBtn" src="img/sneakers/close.svg" alt="Close"/></div></div>
+                  <div className="cartTotalBlock">
+                      <ul>
+                          <li><span>Итого: </span>
+                              <div></div>
+                              <b>21 498 руб. </b></li>
+                          <li><span>Налог 5%:</span>
+                              <div></div>
+                              <b>1074 руб. </b></li>
+                      </ul>
+                      <button>Оформить заказ <img src="img/sneakers/arrow.svg" alt=""/></button>
+                  </div>
               </div>
           </div>
           <header className="d-flex justify-between align-center p-40">
